ae0bd781a4074f72c7db6f49e8aa5e2c3859e17a
Gudmunduro <goh140400@gmail.com>
2019-04-02 18:02:52 +0000

    Added part 3

--- Sources/Reiknirit6/main.swift
+++ Sources/Reiknirit6/main.swift
@@ -3,7 +3,8 @@ import Common
 
 while true {
     let menu = Menu(title: "Reiknirit - Verkefni 5", options: [
-        (text: "Partur 1", option: runPart1),
+        (text: "Liður 1", option: runPart1),
+        (text: "Liður 3", option: runPart3),
         (text: "Exit", option: {
             exit(0)
         })
--- Sources/Reiknirit6/part3.swift
+++ Sources/Reiknirit6/part3.swift
@@ -1,72 +1,256 @@
 import Common
 
+
+// ***
+// Node
+// ***
 class Node {
 
-    let value: Int
+    var value: Int
     var left: Node?
     var right: Node?
 
-    init(_ value: Int)
-    {
+    init(_ value: Int) {
         self.value = value
     }
+}
 
-    func insert(_ data: Int) -> Bool
-    {
-        guard self.value != data else {
+// Functions
+extension Node {
+
+    var hasAnyChildren: Bool {
+        return right != nil || left != nil
+    }
+
+    var hasBothChildren: Bool {
+        return right != nil && left != nil
+    }
+
+    func insert(_ value: Int) -> Bool {
+        guard self.value != value else {
             return false
         }
-        if self.value > data {
+
+        if self.value > value {
             if let left = self.left {
-                return left.insert(data)
+                return left.insert(value)
             } else {
-                self.left = Node(data)
+                self.left = Node(value)
                 return true
             }
         } else {
             if let right = self.right {
-                return right.insert(data)
+                return right.insert(value)
             } else {
-                self.right = Node(data)
+                self.right = Node(value)
                 return true
+            }
+        }
+    }
+
+    func findParent(_ value: Int) -> Node? {
+        if self.value > value {
+            guard let left = self.left else {
+                return nil
+            }
+            if value == left.value {
+                return self
+            } else {
+                return left.findParent(value)
+            }
+        } else {
+            guard let right = self.right else {
+                return nil
+            }
+            if value == right.value {
+                return self
+            } else {
+                return right.findParent(value)
+            }
+        }
+        return nil
+    }
+
+    func preOrderPrint() {
+        print(value, terminator: ", ")
+        if let left = self.left {
+            left.preOrderPrint()
+        }
+        if let right = self.right {
+            right.preOrderPrint()
+        }
+    }
+
+    func postOrderPrint() {
+        if let left = self.left {
+            left.preOrderPrint()
+        }
+        if let right = self.right {
+            right.preOrderPrint()
+        }
+        print(value, terminator: ", ")
+    }
+
+    private func findInorderSuccessor() -> Node
+    {
+        var currentLowest = self
+        if let left = self.left {
+            let ios = left.findInorderSuccessor()
+            if ios.value < value {
+                currentLowest  = ios
+            }
+        }
+        if let right = self.right {
+            let ios = right.findInorderSuccessor()
+            if ios.value < value {
+                currentLowest  = ios
+            }
+        }
+        return currentLowest
+    }
+
+    private func deleteThis(parent: Node) {
+        if !hasAnyChildren {
+            if parent.left === self {
+                parent.left = nil
+            } else if parent.right === self {
+                parent.right = nil
+            }
+        } else if hasAnyChildren && !hasBothChildren {
+            if left != nil {
+                if parent.left === self {
+                    parent.left = left
+                } else if parent.right === self {
+                    parent.right = left
+                }
+            }
+            if right != nil {
+                if parent.left === self {
+                    parent.left = right
+                } else if parent.right === self {
+                    parent.right = right
+                }
+            }
+        } else {
+            let ios = right!.findInorderSuccessor()
+            value = ios.value
+            guard let parent = try! findParent(ios.value) else {
+                return
+            }
+            ios.deleteThis(parent: parent)
+        }
+    }
+
+    func delete(_ value: Int) -> Bool {value
+        if self.value > value {
+            guard let left = self.left else {
+                return false
+            }
+
+            if left.value == value {
+                left.deleteThis(parent: self)
+                return true
+            } else {
+                return left.delete(value)
+            }
+        } else {
+            guard let right = self.right else {
+                return false
+            }
+
+            if right.value == value {
+                right.deleteThis(parent: self)
+                return true
+            } else {
+                return right.delete(value)
             }
         }
     }
 }
 
+
+
+// ***
+// Tree
+// ***
 class Tree {
 
     var root: Node?
 
-    func insert(data: Int) -> Bool {
+    init() { }
+
+    init(root: Node) {
+        self.root = root
+    }
+
+    init(value: Int) {
+        self.root = Node(value)
+    }
+}
+
+// Functions
+extension Tree {
+
+    func insert(_ value: Int) -> Bool {
         if var root = self.root {
-            return root.insert(data)
+            return root.insert(value)
         } else {
-            self.root = Node(data)
+            self.root = Node(value)
             return true
         }
     }
 
-    func preOrderPrint()
-    {
-
+    func preOrderPrint() {
+        if let root = self.root {
+            root.preOrderPrint()
+        } else {
+            print("Tréið er tómt")
+            
+        }
     }
 
-    func postOrderPrint()
-    {
-
+    func postOrderPrint() {
+        if let root = self.root {
+            root.postOrderPrint()
+        } else {
+            print("Tréið er tómt")
+            
+        }
     }
 
-    func delete()
-    {
-
+    func delete(_ value: Int) -> Bool {
+        if let root = self.root {
+            if root.value == value {
+                self.root = root.left
+                return true
+            } else {
+                return root.delete(value)
+            }
+        } else {
+            print("Tréið er tómt")
+            return false
+        }
     }
 
-    func deleteTree()
+    func deleteTree() {
+        root = nil // Eyðir reference á rótina og lætur ARC(Automatic Reference Counting) sjá um restina
     }
 }
 
+
+
+// ***
+// Run func
+// ***
+
 func runPart3()
 {
-
+    let tree = Tree()
+    tree.insert(4)
+    tree.insert(3)
+    tree.insert(2)
+    tree.insert(5)
+    tree.insert(6)
+    tree.delete(5)
+    tree.postOrderPrint()
 }
